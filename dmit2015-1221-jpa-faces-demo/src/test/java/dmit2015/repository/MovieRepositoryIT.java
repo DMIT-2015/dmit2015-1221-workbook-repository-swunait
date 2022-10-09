f542ea1b1a9baa526cefda1a9bd54f614e3b37a5
Sam Wu <swu@nait.ca>
2022-10-09 14:11:15 -0600

    Correct file name for aquillian.xml and updated MovieRepositoryIT to use alternate testing techique

--- dmit2015-1221-jpa-faces-demo/src/test/java/dmit2015/repository/MovieRepositoryIT.java
+++ dmit2015-1221-jpa-faces-demo/src/test/java/dmit2015/repository/MovieRepositoryIT.java
@@ -1,13 +1,15 @@
 package dmit2015.repository;
 
-import dmit2015.entity.Movie;
 import ca.nait.dmit.listener.MovieApplicationStartupListener;
 import common.config.ApplicationConfig;
+import dmit2015.entity.Movie;
+import jakarta.annotation.Resource;
 import jakarta.inject.Inject;
+import jakarta.transaction.NotSupportedException;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
-import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
-import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
@@ -29,9 +31,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(ArquillianExtension.class)                  // Run with JUnit 5 instead of JUnit 4
 public class MovieRepositoryIT {
-
     @Inject
     private MovieRepository _movieRepository;
+
+    @Resource
+    private UserTransaction _beanManagedTransaction;
 
     @Deployment
     public static WebArchive createDeployment() {
@@ -41,18 +45,20 @@ public class MovieRepositoryIT {
                 .addAsLibraries(pomFile.resolve("com.h2database:h2:2.1.214").withTransitivity().asFile())
                 .addAsLibraries(pomFile.resolve("com.microsoft.sqlserver:mssql-jdbc:11.2.1.jre17").withTransitivity().asFile())
 //                .addAsLibraries(pomFile.resolve("com.oracle.database.jdbc:ojdbc11:21.7.0.0").withTransitivity().asFile())
+//                .addAsLibraries(pomFile.resolve("org.hibernate.orm:hibernate-spatial:6.1.3.Final").withTransitivity().asFile())
                 .addAsLibraries(pomFile.resolve("org.hamcrest:hamcrest:2.2").withTransitivity().asFile())
                 .addClass(ApplicationConfig.class)
                 .addClasses(MovieApplicationStartupListener.class)
                 .addClasses(Movie.class, MovieRepository.class)
+                .addAsResource("data/csv/movies.csv")
                 .addAsResource("META-INF/persistence.xml")
                 .addAsResource("META-INF/beans.xml");
     }
 
-    @Transactional(TransactionMode.ROLLBACK)
     @Order(3)
     @Test
-    void shouldCreate() {
+    void shouldCreate() throws SystemException, NotSupportedException {
+        _beanManagedTransaction.begin();
         // Arrange
         Movie newMovie = new Movie();
         newMovie.setGenre("Horror");
@@ -77,6 +83,8 @@ public class MovieRepositoryIT {
         assertEquals(0, createdDateTimeDifference);
 
         assertNull(newMovie.getUpdatedDateTime());
+
+        _beanManagedTransaction.rollback();
     }
 
     @Order(2)
@@ -122,10 +130,9 @@ public class MovieRepositoryIT {
         assertEquals(LocalDate.parse("1959-04-15", formatter).toString(), lastMovie.getReleaseDate().toString());
     }
 
-    @Transactional(TransactionMode.ROLLBACK)
     @Order(4)
     @Test
-    void shouldUpdate()  {
+    void shouldUpdate() throws SystemException, NotSupportedException {
         // Arrange - Create a new Movie then update the same Movie
         Movie newMovie = new Movie();
         newMovie.setGenre("Adventure");
@@ -134,11 +141,13 @@ public class MovieRepositoryIT {
         newMovie.setTitle("JDK 17 Release Party");
         newMovie.setReleaseDate(LocalDate.parse("2021-09-14"));
         _movieRepository.add(newMovie);
+
         final Long editId = newMovie.getId();
         Optional<Movie> optionalMovie = _movieRepository.findOptionalById(editId);
         assertTrue(optionalMovie.isPresent());
         Movie existingMovie = optionalMovie.get();
         assertNotNull(existingMovie);
+
         // Act - change the genre, title, rating, price, and release date
         existingMovie.setGenre("Action");
         existingMovie.setTitle("JDK 18 Release Party");
@@ -159,15 +168,18 @@ public class MovieRepositoryIT {
         assertEquals(existingMovie.getRating(), updatedMovie.getRating());
         assertEquals(existingMovie.getReleaseDate(), updatedMovie.getReleaseDate());
 
-//        assertNotNull(updatedMovie.getUpdatedDateTime());
-//        long updatedDateTimeDifference = updatedMovie.getUpdatedDateTime().until(LocalDateTime.now(), ChronoUnit.MINUTES);
-//        assertEquals(0, updatedDateTimeDifference);
+        assertNotNull(updatedMovie.getUpdatedDateTime());
+        long updatedDateTimeDifference = updatedMovie.getUpdatedDateTime().until(LocalDateTime.now(), ChronoUnit.MINUTES);
+        assertEquals(0, updatedDateTimeDifference);
 
+        // Delete the entity that was added to test the update method
+        _movieRepository.delete(updatedMovie);
     }
 
     @Order(5)
     @Test
-    void shouldDelete() {
+    void shouldDelete() throws SystemException, NotSupportedException {
+        _beanManagedTransaction.begin();
         // Arrange
         final Long editId = 3L;  // for Ghostbusters 2
         Optional<Movie> optionalMovie = _movieRepository.findOptionalById(editId);
@@ -179,5 +191,7 @@ public class MovieRepositoryIT {
         optionalMovie = _movieRepository.findOptionalById(editId);
         // Assert
         assertTrue(optionalMovie.isEmpty());
+
+        _beanManagedTransaction.rollback();
     }
 }
