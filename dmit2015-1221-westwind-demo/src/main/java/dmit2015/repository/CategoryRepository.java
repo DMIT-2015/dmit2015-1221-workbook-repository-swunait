40fb664c72d63f5954657342ee59ea808daa6815
Sam Wu <swu@nait.ca>
2022-10-27 23:14:31 -0600

    Fixed CategoryQueryListController to call repository method instead of using Stream API.

--- dmit2015-1221-westwind-demo/src/main/java/dmit2015/repository/CategoryRepository.java
+++ dmit2015-1221-westwind-demo/src/main/java/dmit2015/repository/CategoryRepository.java
@@ -5,12 +5,44 @@ import dmit2015.entity.Category;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @ApplicationScoped
 @Transactional
 public class CategoryRepository extends AbstractJpaRepository<Category, Integer> {
 
     public CategoryRepository() {
         super(Category.class);
+    }
+
+    public List<String> findNamesByPattern(String namePattern) {
+        return getEntityManager()
+                .createQuery("""
+                SELECT c
+                FROM Category c
+                WHERE c.categoryName LIKE :nameValue
+                ORDER BY c.categoryName""", String.class)
+                .setParameter("nameValue", "%" + namePattern + "%")
+                .getResultList();
+    }
+
+    public Optional<Category> findByName(String name) {
+        Optional<Category> optionalCategory;
+
+        try {
+            Category querySingleResult = getEntityManager()
+                    .createQuery("""
+                        SELECT c 
+                        FROM Category c 
+                        WHERE c.categoryName = :nameValue""", Category.class)
+                    .setParameter("nameValue", name)
+                    .getSingleResult();
+            optionalCategory = Optional.of(querySingleResult);
+        } catch (Exception ex) {
+            optionalCategory = Optional.empty();
+        }
+        return optionalCategory;
     }
 
 }
--- dmit2015-1221-westwind-demo/src/main/java/dmit2015/view/CategoryQueryListController.java
+++ dmit2015-1221-westwind-demo/src/main/java/dmit2015/view/CategoryQueryListController.java
@@ -2,7 +2,6 @@ package dmit2015.view;
 
 import dmit2015.entity.Category;
 import dmit2015.repository.CategoryRepository;
-import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -30,24 +29,32 @@ public class CategoryQueryListController implements Serializable {
     private Category querySingleResult;
 
     public List<String> completeText(String query) {
-        return _categoryRepository
-                .findAll()
-                .stream()
-                .filter(currentCategory -> currentCategory.getCategoryName().toLowerCase().contains(query.toLowerCase()))
-                .map(currentCategory -> currentCategory.getCategoryName())
-                .toList();
+//        return _categoryRepository
+//                .findAll()
+//                .stream()
+//                .filter(currentCategory -> currentCategory.getCategoryName().toLowerCase().contains(query.toLowerCase()))
+//                .map(currentCategory -> currentCategory.getCategoryName())
+//                .toList();
+
+        // The code above was a temporary workaround the issue where we didn't have the time to code a method in the respository to return the required data
+
+        return _categoryRepository.findNamesByPattern(query);
     }
 
     public void onSearch() {
         try {
-            querySingleResult = _categoryRepository
-                    .findAll()
-                    .stream()
-                    .filter(currentCategory -> currentCategory.getCategoryName().equalsIgnoreCase(querySearchValue))
-                    .findFirst()
-                    .orElseThrow();
-        } catch(Exception ex) {
+//            querySingleResult = _categoryRepository
+//                    .findAll()
+//                    .stream()
+//                    .filter(currentCategory -> currentCategory.getCategoryName().equalsIgnoreCase(querySearchValue))
+//                    .findFirst()
+//                    .orElseThrow();
 
+            // The code above was a temporary workaround the issue where we didn't have the time to code a method in the respository to return the required data
+
+            querySingleResult = _categoryRepository.findByName(querySearchValue).orElseThrow();
+        } catch(Exception ex) {
+            Messages.addGlobalError("There are no result matching {0}", querySearchValue);
         }
     }
 
